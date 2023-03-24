4dc43158a93d02a8a4a551b65f3bbdd1e4a31f78
Kaddu Livingstone <kaddulivingstone@Kaddus-MacBook-Pro.local>
2023-03-24 19:21:19 +0300

    user side category vote api development

--- controllers/vote.controller.js
+++ controllers/vote.controller.js
@@ -5,7 +5,24 @@ const Candidate = models.Candidate
 const Category = models.Category
 const Vote = models.Vote
 
-const Index = async (req, res) => {
+const Index = async (req, res)=>{
+ const page = parseInt(req.query.page) || 1; // current page number
+    const limit = parseInt(req.query.limit) || 10; // number of items per page
+    const offset = (page - 1) * limit; // offset of the first item to return
+
+const categoriesWithCandidates = await Category.findAll({
+  include: {
+    model: Candidate,
+    as: 'candidates',
+    required: true
+  },
+ limit,
+ offset
+});
+return res.status(200).send(categoriesWithCandidates)
+}
+
+const userVotes = async (req, res) => {
     const page = parseInt(req.query.page) || 1; // current page number
     const limit = parseInt(req.query.limit) || 10; // number of items per page
     const offset = (page - 1) * limit; // offset of the first item to return
@@ -45,6 +62,7 @@ const Edit = (req, res) => {
 
 module.exports = {
     Index,
+    userVotes,
     Show,
     Edit,
     Store
